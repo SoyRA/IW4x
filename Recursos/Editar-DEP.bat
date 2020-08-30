80d2c3a858e9d751722471713b5134fededf5762
Soy R.A <36781536+SoyRA@users.noreply.github.com>
2020-08-29 23:08:56 -0300

    Cambios menores
    Nada de lo que preocuparse

--- Recursos/Editar-DEP.bat
+++ Recursos/Editar-DEP.bat
@@ -1,58 +1,50 @@
+:: ---------------- Ignora esto ----------------
 @ECHO OFF
 TITLE Prevencion de ejecutacion de datos (DEP)
 CLS
- 
- 
-:: Aviso
+:: ------------------- Aviso -------------------
 ECHO AVISO
 ECHO - Debes EJECUTAR COMO ADMINISTRADOR este Archivo.bat
-ECHO - Debes REINICIAR el PC para aplicar los cambios
+ECHO - Debes REINICIAR la PC para aplicar los cambios
 ECHO.
 PAUSE
-CLS
-
-
-:: Menú
+:: ------------------- Menú --------------------
 :MENU
+CLS
 ECHO [ 01 ] Desactivar DEP
 ECHO [ 02 ] Activar DEP
 ECHO [ 03 ] Restaurar a los valores por defecto
 ECHO [ 04 ] Verificar el estado de DEP
 ECHO.
- 
-SET /p ver= Usted quiere 0
-IF %ver%==1 GOTO OFF
-IF %ver%==2 GOTO ON
-IF %ver%==3 GOTO DEFAULT
-IF %ver%==4 GOTO STATUS
- 
- 
-:: Desactivar DEP
+SET /P VER=Usted quiere 0
+IF NOT %VER%=="" SET VER=%VER:~0,1%
+IF %VER%==1 GOTO OFF
+IF %VER%==2 GOTO ON
+IF %VER%==3 GOTO DEFAULT
+IF %VER%==4 GOTO STATUS
+GOTO Menu
+:: ----------------- Opción 01 -----------------
 :OFF
 CLS
 bcdedit -set {current} nx AlwaysOff
 ECHO.
 PAUSE
 EXIT
- 
- 
-:: Activar DEP
+:: ----------------- Opción 02 -----------------
 :ON
 CLS
 bcdedit -set {current} nx AlwaysOn
 ECHO.
 PAUSE
 EXIT
-
-
+:: ----------------- Opción 03 -----------------
 :DEFAULT
 CLS
 bcdedit /deletevalue nx
 ECHO.
 PAUSE
 EXIT
-
-
+:: ----------------- Opción 04 -----------------
 :: Verificar el estado del DEP
 :: 0 - bcdedit -set {current} nx AlwaysOff
 :: 1 - bcdedit -set {current} nx AlwaysOn
